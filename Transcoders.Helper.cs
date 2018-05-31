02fdfc88ab2d67561ecef073c4b2d18ef19d1808
Quynh Nguyen <mr.quynh@gmail.com>
2018-05-31 21:46:44 +0700

    Upgrade to latest components to get better performance

--- Transcoders.Helper.cs
+++ Transcoders.Helper.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Text.RegularExpressions;
 
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("VIEApps.Components.XUnitTests")]
+//[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("VIEApps.Components.XUnitTests")]
 
 namespace Enyim.Caching.Memcached
 {
