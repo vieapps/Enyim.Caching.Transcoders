d7987cb22a8b4d673c64b4663ed03200a5c1939a
Quynh Nguyen <mr.quynh@gmail.com>
2019-03-29 17:56:19 +0700

    Refactoring

--- Transcoders.Helper.cs
+++ Transcoders.Helper.cs
@@ -9,6 +9,7 @@ namespace Enyim.Caching.Memcached
 	{
 		static readonly Regex SubtractFullNameRegex = new Regex(@", Version=\d+.\d+.\d+.\d+, Culture=\w+, PublicKeyToken=\w+", RegexOptions.Compiled);
 
-		internal static string BuildTypeName(Type type) => SubtractFullNameRegex.Replace(type.AssemblyQualifiedName, "");
+		internal static string BuildTypeName(Type type)
+			=> SubtractFullNameRegex.Replace(type.AssemblyQualifiedName, "");
 	}
 }
