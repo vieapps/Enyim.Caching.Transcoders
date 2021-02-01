b46c9206db02bc598a8e05af5dac3cae5815f08c
Quynh Nguyen <mr.quynh@gmail.com>
2021-02-01 08:50:56 +0700

    (changes): change the assembly name, only support .NET 5 and .NET Standard 2.0

--- Bson.cs
+++ Bson.cs
@@ -1,8 +1,4 @@
 using System;
-using System.IO;
-using System.Text;
-using System.Collections.Generic;
-using System.Collections.Concurrent;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Bson;
 using CacheUtils;
