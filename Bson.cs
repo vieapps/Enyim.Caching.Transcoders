212336590c74ac1513ea905bb9c27c3f0b27824f
Quynh Nguyen <mr.quynh@gmail.com>
2026-03-09 01:00:17 +0700

    (improv): latest components

--- Bson.cs
+++ Bson.cs
@@ -7,12 +7,14 @@ namespace Enyim.Caching.Memcached
 {
 	public class BsonTranscoder : DefaultTranscoder
 	{
+		static JsonSerializer Serializer { get; } = new JsonSerializer();
+
 		protected override ArraySegment<byte> SerializeObject(object value)
 		{
 			using (var stream = Helper.CreateMemoryStream())
 			using (var writer = new BsonDataWriter(stream))
 			{
-				new JsonSerializer().Serialize(writer, value);
+				Serializer.Serialize(writer, value);
 				return new ArraySegment<byte>(stream.ToBytes());
 			}
 		}
@@ -21,7 +23,7 @@ namespace Enyim.Caching.Memcached
 		{
 			using (var stream = Helper.CreateMemoryStream(value.Array, value.Offset, value.Count))
 			using (var reader = new BsonDataReader(stream))
-				return new JsonSerializer().Deserialize(reader);
+				return Serializer.Deserialize(reader);
 		}
 	}
 }
